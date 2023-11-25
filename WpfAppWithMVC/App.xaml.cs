475ec0d69ceb299aa2208a8a331b2cc958de7804
Justin Wise <justin@thewiseguys.org>
2023-11-25 15:01:18 -0700

    initial repository commit

---
+++ WpfAppWithMVC/App.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+using System.Windows;
+
+namespace WpfAppWithMVC
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            SetupHosting();
+            base.OnStartup(e);
+        }
+
+        private static void SetupHosting()
+        {
+            var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+            {
+                ContentRootPath = Directory.GetCurrentDirectory()
+            });
+
+            builder.WebHost.UseUrls("http://*:9268;");
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddMvc().AddRazorRuntimeCompilation();
+
+            var app = builder.Build();
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            var staticFileDirectory = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+            var opts = new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(staticFileDirectory)
+            };
+            app.UseStaticFiles(opts);
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Start();
+        }
+
+    }
+
+}
---
+++ WpfAppWithMVC/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace WpfAppWithMVC
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var link = "http://localhost:9268";
+
+            var ps = new ProcessStartInfo(link)
+            {
+                UseShellExecute = true,
+                Verb = "open"
+            };
+            Process.Start(ps);
+        }
+    }
+}
